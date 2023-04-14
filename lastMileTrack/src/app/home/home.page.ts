66a3253f0bb8a35e11e16547167bdbef46b9fadd
Abhinav2903 <abhinavpareek532@gmail.com>
2023-04-14 19:13:21 +0200

    Task Detail Page added

--- lastMileTrack/src/app/home/home.page.ts
+++ lastMileTrack/src/app/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import * as moment from 'moment';
 import { StorageKeys } from '../constants/storageVariable.enum';
+import { TaskDetailPage } from '../task-detail/task-detail.page';
 
 @Component({
   selector: 'app-home',
@@ -15,33 +16,64 @@ import { StorageKeys } from '../constants/storageVariable.enum';
 })
 export class HomePage {
   showTaskForm: boolean | undefined;
-  task: string = "";
-  startTime: string = new Date().toISOString();
+  task: string = '';
+  startTime= new Date();
   tasks: any[] = [];
+  isVisible=false;
+  isStopVisible=true;
 
+  endTime!: Date;
+  elapsedTime!: number;
+  timerInterval: any;
 
   // storageVariable
   daytaskCounter = 0; // can be incremented regularly by stroing in the storage
-  newDay:string| undefined;// can be ommited
+  newDay: string | undefined; // can be ommited
 
   @ViewChild('myForm', { static: false })
   myForm!: ElementRef<HTMLFormElement>;
-  
-  
+
   submitForm() {
     this.myForm.nativeElement.submit();
   }
 
-  constructor(private toastController: ToastController) {}
+  constructor(
+    private toastController: ToastController,
+    private navCtrl: NavController
+  ) {}
 
-  async onClick() {
-    const toast = await this.toastController.create({
-      message: 'Timer Stoped',
-      duration: 1500,
-      position: 'bottom',
-    });
+  onClick(event: Event) {
+    this.showToast('Timer Stopped!');
+    event.stopPropagation();
+  }
 
-    await toast.present();
+  onStop(event: Event,task: any) {
+    this.showToast('Timer Stopped!');
+    event.stopPropagation();
+    // this.isStopVisible = false;
+    // this.isVisible = true;
+    task.isShowIcon = !task.isShowIcon;
+    this.stopTimer();
+  }
+
+  onRestart(event: Event) {
+    this.showToast('Timer Stopped!');
+    event.stopPropagation();
+  }
+
+  onComplete(event: Event) {
+    this.showToast('Task is Completed!');
+    event.stopPropagation();
+  }
+
+  showToast(msg: any) {
+    this.toastController
+      .create({
+        message: msg,
+        duration: 1500,
+        position: 'bottom',
+      })
+      .then((toast) => toast.present());
   }
 
   showForm() {
@@ -51,28 +83,59 @@ export class HomePage {
   addTask() {
     console.log(this.startTime);
     console.log(this.task, this.startTime);
-    if(this.task==""){
-      this.daytaskCounter = this.daytaskCounter+1;
-      this.task = "Task"+this.daytaskCounter
-
+    if (this.task == '') {
+      this.daytaskCounter = this.daytaskCounter + 1;
+      this.task = 'Task' + this.daytaskCounter;
     }
     this.tasks.push({
       task: this.task,
       startTime: moment(this.startTime, 'hh:mm A').toLocaleString(),
+      isShowIcon: true,
     });
     this.showTaskForm = false;
     this.task = '';
-    console.log(this.showTaskForm)
+    console.log(this.showTaskForm);
+    this.startTimer()
+
   }
 
   //routing to the list page
-  async redirectToListData(){
-    const toast = await this.toastController.create({
-      message: 'Redirection',
-      duration: 1500,
-      position: 'bottom',
-    });
+  redirectToListData() {
+    this.navCtrl.navigateForward('/task-detail');
+  }
 
-    await toast.present();
+
+  
+  startTimer() {
+    this.startTime = new Date();
+    this.timerInterval = setInterval(() => {
+      this.calculateElapsedTime();
+    }, 1000);
+  }
+  
+  calculateElapsedTime() {
+    this.endTime = new Date();
+    this.elapsedTime = Math.round((this.endTime.getTime() - this.startTime.getTime()) / 1000);
+    console.log(this.elapsedTime)
+  }
+  
+  stopTimer() {
+    clearInterval(this.timerInterval);
+    this.calculateElapsedTime();
+  }
+  
+  resetTimer() {
+    clearInterval(this.timerInterval);
+    this.elapsedTime = 0;
+  }
+
+  //use gps
+  calculateStartLocation(){
+
+  }
+
+  // use gps
+  calculateEndLocation(){
+
   }
 }
---
+++ lastMileTrack/src/app/task-detail/task-detail.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-task-detail',
+  templateUrl: './task-detail.page.html',
+  styleUrls: ['./task-detail.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class TaskDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
